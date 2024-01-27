578c839802b3a159b6c8853edb862a8f77fc9991
shai <shai239@gmail.com>
2024-01-27 17:29:15 +0200

    fixed naming that made building crash

--- src/Components/home-page/home-page.component.ts
+++ src/Components/home-page/home-page.component.ts
@@ -8,7 +8,7 @@ import { HtmlParser } from '@angular/compiler';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrl: './coins-page.component.css',
+  styleUrl: './home-page.component.css',
   animations: [fadeInOut],
 })
 export class HomePageComponent {
