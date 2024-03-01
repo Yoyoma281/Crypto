aa914554bab3992884105aa65f0c330b62e497e5
shai <shai239@gmail.com>
2024-03-01 19:06:43 +0200

    another try

--- environment.ts
+++ environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 
 export const environment = {
-    production: false,
+    production: true,
     apiUrl: 'https://newsapi.org/v2/everything',
     apiKey: process.env['NEWS_API_KEY'],
 };
