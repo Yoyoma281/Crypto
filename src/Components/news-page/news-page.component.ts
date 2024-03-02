bf21e775135513ddd7894718d8b871b56d930c92
shai <shai239@gmail.com>
2024-03-02 21:25:48 +0200

    NewsPage

--- src/Components/news-page/news-page.component.ts
+++ src/Components/news-page/news-page.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsApiService } from '../../services/NewsApi/news-api.service';
+import { Article } from '../../models/Articles';
+
 @Component({
   selector: 'app-news-page',
   templateUrl: './news-page.component.html',
   styleUrl: './news-page.component.css',
 })
-export class NewsPageComponent implements OnInit{
-  constructor(private News: NewsApiService) {}
+export class NewsPageComponent implements OnInit {
+  private articles: Article[] = []
+  TopArticles: Article[] = []
+
+  constructor(private News: NewsApiService) { }
 
   ngOnInit(): void {
     this.GetNews();
   }
 
   GetNews() {
-    this.News.getNews().subscribe((data) =>{
-      console.log("news: ", data)
+    this.News.getNews().subscribe((data) => {
+      this.articles = data.articles
+      console.log("news comp: ", this.articles)
+      this.TopArticles = this.articles.slice(0, 3)
     })
   }
 }
--- src/services/NewsApi/news-api.service.ts
+++ src/services/NewsApi/news-api.service.ts
@@ -1,23 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Article } from '../../models/Articles';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsApiService {
   private apiUrl = 'https://newsapi.org/v2/everything';
-  private apiKey = process.env['NEWS_API_KEY'];
+  private apiKey = '2d68032cb7464311b4a5ac763d8b667c';
   constructor(private http: HttpClient) { }
 
   getNews(): Observable<any> {
-    const fromDate = '2024-02-01';
+    const fromDate = '2024-02-03';
     const sortBy = 'publishedAt';
     const query = 'Binance';
 
     const url = `${this.apiUrl}?q=${query}&from=${fromDate}&sortBy=${sortBy}&apiKey=${this.apiKey}`;
 
-    return this.http.get<any>(url);
+    return this.http.get<Article>(url);
   }
 }
 
